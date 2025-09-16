53df0ed7d3d4cd93ab1b86029d4d13179a8f58bd
bueno <justhinvalleclass@gmail.com>
2025-09-16 11:19:46 -0500

    Clase

---
+++ SEMANA1_C#/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA1_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer6();
+            Console.ReadKey();
+        }
+        static void ejer1()
+        {
+            string nombre, carrera;
+
+            Console.Write("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.Write("Ingrese su Carrera: ");
+            carrera = Console.ReadLine();
+
+            Console.Write($"\n{nombre}, bienvenido a FA de {carrera}");
+
+
+        }
+        static void ejer2()
+        {
+            Console.WriteLine("\"Yordan\"");
+
+        }
+        static void ejer3()
+        {
+            Console.WriteLine("Ingrese numero x: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese numero y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            double resu = (double)x / (double)y;
+
+            Console.WriteLine($"Suma: {x + y}");
+            Console.WriteLine($"Resta: {x - y}");
+            Console.WriteLine($"Multiplicacion: {x * y}");
+            Console.WriteLine($"Division: {resu}");
+        }
+        static void ejer4()
+        {
+            Console.Write("Ingrese un numero decimal: ");
+            double num = Convert.ToDouble(Console.ReadLine());
+
+            double raiz2 = Math.Sqrt(num);
+            int redo = (int)Math.Round(num, 0);
+            double cubo = Math.Pow(num, 3);
+            double raiz3 = Math.Pow(num, 1/3d);
+
+            Console.WriteLine($"Raiz 2: {raiz2}");
+            Console.WriteLine($"Redondeado: {redo}");
+            Console.WriteLine($"Al cubo: {cubo}");
+            Console.WriteLine($"Raiz 3: {raiz3}");
+
+        }
+        static void ejer5()
+        {
+            Console.Write("Ingrese un numero");
+            string num = Console.ReadLine();
+
+            int entero = int.Parse(num);
+            double deci = double.Parse(num);
+
+            Console.WriteLine("Resto: " + (entero % 2));
+            Console.WriteLine("Division:" + (deci / 3));
+        }
+        static void ejer6()
+        {
+            Console.Write("Ingrese los segundos: ");
+            int segundos = Convert.ToInt32(Console.ReadLine());
+
+            int horas = segundos / 3600;
+            int minutos = (segundos % 3600) / 60;
+            int segundosrest = segundos % 60;
+
+            Console.WriteLine("Horas: " + horas);
+            Console.WriteLine("Minutos: " + minutos);
+            Console.WriteLine("Segundos: " + segundosrest);
+        }
+
+    }
+}
---
+++ SEMANA1_PYHON/SEMANA1_PYHON.py
@@ -0,0 +1,52 @@
+﻿import math
+
+
+def ejer1():
+    nombre = input("Ingrese su nombreP: ")
+    carrera = input("Ingrese su carrera: ")
+
+    print(f"\n{nombre}, bienvenido a FA de {carrera}")
+
+def ejer2():
+    print("\"Bueno\"")
+
+
+def ejer3():
+        x = int(input("Ingres el valor de x: "))
+        y= int(input("Ingres el valor de y: "))
+
+        print("Suma: ", (x + y))
+        print("Resta: ", (x - y))
+        print("Multiplicacion: ", (x * y))
+        print("Division: ", (x / y))
+
+
+import math
+def ejer4():
+    num = float(input("Ingrese un numero decimal:"))
+
+    print("Raiz 2: ", math.sqrt(num))
+    print("Redondeado: ", round(num,0))
+    print("Al cubo: ", math.pow(num,3))
+    print("Raiz 3: ", num**(1/3))
+
+
+def ejer5():
+    num = float(input("Ingrese un número: "))
+
+    print("Resto:", int(num) % 2)
+    print("División:", num / 3)
+
+
+def ejer6():
+    segundos = int(input("Ingrese los segundos: "))
+
+    horas = segundos // 3600
+    minutos = (segundos % 3600) // 60
+    segundosrest = segundos % 60
+
+    print("Horas:", horas)
+    print("Minutos:", minutos)
+    print("Segundos:", segundosrest)
+
+ejer6()
